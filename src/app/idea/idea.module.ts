3633356c60ab4bb5d6f5b1270bfb38eecc86056c
Amita kale <amitakale297@gmail.com>
2021-03-21 19:15:13 +0530

    added new idea component

--- src/app/idea/idea.module.ts
+++ src/app/idea/idea.module.ts
@@ -5,10 +5,11 @@ import { IdeaRoutingModule } from './idea-routing.module';
 import { ViewIdeaComponent } from './view-idea/view-idea.component';
 import { MyIdeasComponent } from './my-ideas/my-ideas.component';
 import { ViewAllIdeasComponent } from './view-all-ideas/view-all-ideas.component';
+import { NewIdeaComponent } from './new-idea/new-idea.component';
 
 
 @NgModule({
-  declarations: [ViewIdeaComponent, MyIdeasComponent, ViewAllIdeasComponent],
+  declarations: [ViewIdeaComponent, MyIdeasComponent, ViewAllIdeasComponent, NewIdeaComponent],
   imports: [
     CommonModule,
     IdeaRoutingModule
