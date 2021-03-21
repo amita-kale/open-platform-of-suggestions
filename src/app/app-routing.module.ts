efc22463cff47f68e1a6afb3b0da0c94f7485e38
Amita kale <amitakale297@gmail.com>
2021-03-21 18:27:47 +0530

    default routing to home module component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { LayoutComponent } from './layout/layout.component';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     component: LayoutComponent,
     children: [
       {
