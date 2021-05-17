804b29210d0e73fae5c8bb15be25dccfb4223294
Amita kale <amitakale297@gmail.com>
2021-05-17 12:57:56 +0530

    routing and pending for accepatance routing done

--- src/app/idea/idea.module.ts
+++ src/app/idea/idea.module.ts
@@ -7,10 +7,11 @@ import { ViewAllIdeasComponent } from './view-all-ideas/view-all-ideas.component
 import { NewIdeaComponent } from './new-idea/new-idea.component';
 import { MyIdeaComponent } from './my-idea/my-idea.component';
 import { FormsModule } from '@angular/forms';
+import { PendingForAccepatanceComponent } from './pending-for-accepatance/pending-for-accepatance.component';
 
 
 @NgModule({
-  declarations: [ViewIdeaComponent, ViewAllIdeasComponent, NewIdeaComponent, MyIdeaComponent],
+  declarations: [ViewIdeaComponent, ViewAllIdeasComponent, NewIdeaComponent, MyIdeaComponent, PendingForAccepatanceComponent],
   imports: [
     CommonModule,
     IdeaRoutingModule,
---
+++ src/app/idea/pending-for-accepatance/pending-for-accepatance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-for-accepatance',
+  templateUrl: './pending-for-accepatance.component.html',
+  styleUrls: ['./pending-for-accepatance.component.css']
+})
+export class PendingForAccepatanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
