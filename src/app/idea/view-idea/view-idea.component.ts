8eedf5572ce7fbba088647dbbbd3041e374e2d47
Aniket Kale <kaleaniket66@gmail.com>
2021-06-12 19:21:31 +0530

    remove unnecceesary code

--- src/app/idea/view-idea/view-idea.component.ts
+++ src/app/idea/view-idea/view-idea.component.ts
@@ -9,16 +9,7 @@ import { ideaService } from '../idea.service';
   styleUrls: ['./view-idea.component.css'],
 })
 export class ViewIdeaComponent implements OnInit {
-  // rejectvar = false;
   idea: Idea;
-  // ideainfo = {
-  //   title: '',
-  //   description: '',
-  //   benefits: '',
-  //   createdon: null,
-  //   department: '',
-  //   authorname: '',
-  // };
 
   constructor(
     private route: ActivatedRoute,
@@ -27,19 +18,12 @@ export class ViewIdeaComponent implements OnInit {
 
   ngOnInit(): void {
     const id = this.route.snapshot.params.id;
-    // this.ideainfo = this.studentideaService.getSpecificStudentByIndex(id);
     this.ideaService.getIdeaById(id).subscribe((response: any) => {
       this.idea = response;
     });
-    console.log(this.idea);
   }
 
   goBack() {
     history.back();
   }
-
-  // rejectClicked() {
-  //   // this.rejectvar = true;
-  //   // console.log(this.rejectvar);
-  // }
 }
